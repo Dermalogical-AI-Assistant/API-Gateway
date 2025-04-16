466ce5b1d836822fd8ac37986741d73b406b081f
xenwithu <xenwithu@outlook.com>
2025-04-16 20:30:53 +0700

    allow all request

--- src/main/java/org/xenwithu/api_gateway/config/Configurations.java
+++ src/main/java/org/xenwithu/api_gateway/config/Configurations.java
@@ -2,6 +2,8 @@ package org.xenwithu.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -10,10 +12,12 @@ import java.util.Arrays;
 
 @Configuration
 public class Configurations {
+
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5174"));
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
