e0f8cf4097fcabe67d19b4d2ccf293d6f2548b4e
Tram Tran <jasminebkdn@gmail.com>
2025-05-23 10:39:26 +0700

    feat: build docker image

--- src/main/java/org/xenwithu/api_gateway/config/Configurations.java
+++ src/main/java/org/xenwithu/api_gateway/config/Configurations.java
@@ -1,5 +1,7 @@
 package org.xenwithu.api_gateway.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -8,8 +10,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
 @Configuration
 public class Configurations {
 
@@ -17,7 +17,7 @@ public class Configurations {
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5174","http://localhost:5173"));
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
